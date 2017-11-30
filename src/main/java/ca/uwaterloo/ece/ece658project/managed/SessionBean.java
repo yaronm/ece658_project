c6728c0b8f2abc03ed95504d4521c76a15775dfc
C. Ansel Horn <cahorn@uwaterloo.ca>
2017-11-30 12:55:06 -0500

    Add current user display and logout button

--- src/main/java/ca/uwaterloo/ece/ece658project/managed/SessionBean.java
+++ src/main/java/ca/uwaterloo/ece/ece658project/managed/SessionBean.java
@@ -2,13 +2,20 @@ package ca.uwaterloo.ece.ece658project.managed;
 
 import java.io.Serializable;
 
+import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+
+import ca.uwaterloo.ece.ece658project.UserManagerBean;
+import ca.uwaterloo.ece.ece658project.interfaces.User;
 
 @Named
 @SessionScoped
 @SuppressWarnings("serial")
 public class SessionBean implements Serializable {
+
+	@EJB
+	private UserManagerBean userManager;
 
 	private String email;
 
@@ -20,4 +27,17 @@ public class SessionBean implements Serializable {
 		this.email = email;
 	}
 	
+	public User getUser() {
+		return userManager.getUser(getEmail());
+	}
+
+	public boolean isLoggedIn() {
+		return getEmail() != null;
+	}
+
+	public String logout() {
+		setEmail(null);
+		return "index";
+	}
+
 }
