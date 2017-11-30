dfcdc761e262f5f11f9f99b67ad044dc24682f35
C. Ansel Horn <cahorn@uwaterloo.ca>
2017-11-30 13:06:12 -0500

    Update database initialization

--- src/main/java/ca/uwaterloo/ece/ece658project/DatabaseInitialization.java
+++ src/main/java/ca/uwaterloo/ece/ece658project/DatabaseInitialization.java
@@ -32,9 +32,7 @@ public class DatabaseInitialization {
 			// create users
 			String ansel, user1;
 			ansel = "ansel@horn.name";
-			user1 = "user1@example.com";
 			userManager.createUser("Ansel Horn", ansel);
-			userManager.createUser("User1", user1);
 			// create potlucks
 			Long potluck1, potluck2;
 			potluck1 = potluckManager.createPotluck("First Potluck", ansel, "My first potluck!");
@@ -43,12 +41,10 @@ public class DatabaseInitialization {
 			potluckManager.schedule(new Event("Setup", new Date(117, 12, 1, 18, 0), "Setup for the potluck."));
 			potluckManager.schedule(new Event("Potluck", new Date(117, 12, 1, 19, 0), "My house"));
 			potluckManager.acceptInvitation(ansel);
-			potluckManager.invite(user1);
 			potluckManager.addItem("Hamburgers");
 			potluckManager.addItem("Cheese");
 			potluckManager.commitToItem("Cheese", ansel);
 			potluckManager.addRestriction("Vegitarian food only, please!");
-			potluckManager.createPotluck("Second Potluck", ansel, "My second potluck...");
 		} catch (LoginException e) {
 			logger.log(Level.SEVERE, e.getMessage(), e);
 		}
